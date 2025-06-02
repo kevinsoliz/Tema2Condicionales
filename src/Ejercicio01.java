2648cf70d2e5febfea8b2abf4149bfd9617306ea
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-02 20:19:48 +0200

    Expresiones relacionales.

---
+++ src/Ejercicio01.java
@@ -0,0 +1,10 @@
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        System.out.println(4 > 5);
+        System.out.println(3 != 3);
+        System.out.println(4 == 4);
+        System.out.println("Hola".equals("Hola"));
+        System.out.println("hola".equalsIgnoreCase("Hola"));
+        System.out.println(!(5 > 4));
+    }
+}

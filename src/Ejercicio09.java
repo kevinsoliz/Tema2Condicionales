f3b05bde780f676cc861d1159ffd0eeb95130ea8
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-03 22:41:00 +0200

    A ver si puedo usar una sobrecarga aquí

---
+++ src/Ejercicio09.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio09 {
+    static int pedirNumero(Scanner sc){
+        int numero = 0;
+        boolean valid = false;
+        while(!valid){
+            try{
+                System.out.println("Introduce el numero:");
+                numero = sc.nextInt();
+                valid = true;
+            }catch (InputMismatchException e){
+                System.out.println("No es un numero");
+                sc.next();
+            }
+        }
+        return numero;
+    }
+    static String ordernarDecreciente(int a, int b){
+        return a > b ? a + ", " +  b : b + ", " + a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = pedirNumero(sc);
+        int b = pedirNumero(sc);
+        System.out.println(ordernarDecreciente(a,b));
+    }
+}

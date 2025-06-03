b4a17ed6419c8cabead3cdaad972d47bf8d2695d
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-03 20:33:23 +0200

    Nada interesante de momento

---
+++ src/Ejercicio06.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio06 {
+    static int pedirNumero(Scanner sc) {
+        int numero = 0;
+        boolean valido = false;
+        while (!valido) {
+            try{
+                System.out.println("Introduce el numero: ");
+                numero = sc.nextInt();
+                valido = true;
+            }catch(InputMismatchException e){
+                System.out.println("El numero introducido no es valido");
+            }
+        }
+        return numero;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Son iguales: " + ((pedirNumero(sc) == pedirNumero(sc))));
+    }
+}

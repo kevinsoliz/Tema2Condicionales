133ef4ab02c46d10b65f6db5ce7541ccfdbaa8c0
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-14 22:07:45 +0200

    No tenia ni idea de las barras bajas ni de las letras al final

---
+++ src/Types.java
@@ -0,0 +1,7 @@
+public class Types {
+    public static void main(String[] args) {
+        float price = 10.88F;
+        long countViews = 2_000_000_0000L;
+        long count = 3_999_999_999L;
+    }
+}

5f0673bdac61561f5cd895fbe5e179abdbb217ef
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-14 22:09:06 +0200

    Members, fields(atributos), methods

--- src/Types.java
+++ src/Types.java
@@ -1,7 +1,13 @@
+import java.util.Date;
+
 public class Types {
     public static void main(String[] args) {
         float price = 10.88F;
         long countViews = 2_000_000_0000L;
         long count = 3_999_999_999L;
+        //El compilador de java lee los números como int o float solamente.
+        byte age = 20;
+        Date date = new Date();
+        System.out.println(date);
     }
 }

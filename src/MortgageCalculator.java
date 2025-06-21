f8dfd6b777759b395f1a9ad5465c3bc674afcc69
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-21 18:13:39 +0200

    Como lo ha hecho un pro

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MortgageCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double principal;
+        /*double principal;
         double interest;
         int periodMonths;
         double mortgage;
@@ -23,6 +23,26 @@ public class MortgageCalculator {
         double divisor = Math.pow(1 + r, n) - 1;
 
         mortgage = principal * dividendo / divisor;
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));*/
+
+        //Como lo ha hecho Mosh:
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        System.out.print("Principal: ");
+        int principal = sc.nextInt();
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = sc.nextFloat(); //porque los intereses son pequeños.
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period(Years): ");
+        byte years = sc.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR; //the code must be self explanotory
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }

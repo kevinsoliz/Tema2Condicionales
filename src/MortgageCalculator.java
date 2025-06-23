9598f52cf7b44acac59786ed7429d42857fb7429
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-23 08:08:09 +0200

    Morgage con loops, lo que yo hice

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -26,6 +26,7 @@ public class MortgageCalculator {
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));*/
 
         //Como lo ha hecho Mosh:
+        /*
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
@@ -38,6 +39,43 @@ public class MortgageCalculator {
         System.out.print("Period(Years): ");
         byte years = sc.nextByte();
         int numberOfPayments = years * MONTHS_IN_YEAR; //the code must be self explanotory
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
+         */
+        //Ahora con condicionales:
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int principal = 0;
+        while (!(principal > 1000 && principal < 1_000_000)) {
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+            System.out.print("Principal ($1K - $1M): ");
+            principal = sc.nextInt();
+        }
+
+
+        float annualInterest = 0;
+        while (!(annualInterest > 0 && annualInterest <= 30)){
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = sc.nextFloat();
+        }
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        byte years = 0;
+        while (!(years > 0 && years <= 30)){
+            System.out.println("Enter a value between 1 and 30.");
+            System.out.print("Period(Years): ");
+            years = sc.nextByte();
+        }
+
+        int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
